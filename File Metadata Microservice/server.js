8696f9500abc13f4344eed1422666e09555abecf
Raj Aryan <raj.12020162@lpu.in>
2022-01-13 23:06:52 +0530

    Add files via upload

---
+++ File Metadata Microservice/server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+
+var app = express();
+
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function (req, res) {
+	res.sendFile(process.cwd() + "/views/index.html");
+});
+
+var multer = require("multer");
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
+
+app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
+	if (req.file) {
+		res.send({
+			name: req.file.originalname,
+			type: req.file.mimetype,
+			size: req.file.size,
+		});
+	} else {
+		res.send({ error: "no file uploaded" });
+	}
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+	console.log("Your app is listening on port " + port);
+});
